f7bbe0dddd80ebbed4847cda43019bb698d619ab
Daniel Write <daniel@writefamily.com>
2023-09-02 18:45:25 -0400

    implemented basic parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use std::env;
+
+#[derive(Debug)]
+enum Instruction {
+	MoveRight,		// increment the pointer by one
+	MoveLeft,		// decrement the pointer by one
+	Increment,		// increment the memory at pointer by one
+	Decrement,		// decrement the memory at pointer by one
+	Output,			// output byte from memory
+	Input,			// input byte into memory
+	Loop,			// jump past the matching ] if memory is zero
+	Continue,		// jump to matching [ if memory is nonzero
+	Jump,			// set pointer to value in memory
+	Return,			// restore pointer to value before jump
+	Alloc			// with the value of memory, jump to first instance of that many 0s in memory
+}
+
 fn main() {
-	
+	let args: Vec<String> = env::args().collect();
+	let code = &args[1];
+
+	let mut memory: [u8; u8::max_value() as usize] = [0; u8::max_value() as usize];
+	let mut instr: Vec<Instruction> = Vec::with_capacity(code.len());
+
+	for instruction in code.chars() {
+		let instruction = match instruction {
+			'>' => Instruction::MoveRight,
+			'<' => Instruction::MoveLeft,
+			'+' => Instruction::Increment,
+			'-' => Instruction::Decrement,
+			'.' => Instruction::Output,
+			',' => Instruction::Input,
+			'[' => Instruction::Loop,
+			']' => Instruction::Continue,
+			'*' => Instruction::Jump,
+			'&' => Instruction::Return,
+			'?' => Instruction::Alloc,
+			_ => panic!("No such instruction: {}", instruction)
+		};
+
+		instr.push(instruction);
+	}
+
+	let mut pointer: usize = 0;
 }
