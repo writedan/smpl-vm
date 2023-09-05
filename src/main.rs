14d777db94ea62cba5c5df940d610ffa978bd74c
Daniel Write <daniel@writefamily.com>
2023-09-04 23:49:10 -0400

    rebuilt execution code

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
 
 	let mut memory: Vec<u8> = vec![0; u16::max_value() as usize];
 
-	let mut instr: Vec<Instruction> = parse_instructions(code);
+	let instr: Vec<Instruction> = parse_instructions(code);
 	let branches = calculate_branches(&instr).unwrap();
 
 	let mut ptr_jumps: Vec<usize> = vec![0, u16::max_value() as usize];
@@ -18,6 +18,38 @@ fn main() {
 
 	println!("Instructions: {:?}", instr);
 	println!("Branch table: {:?}", branches);
+
+	let mut iter = instr.iter().enumerate();
+	while let Some((position, instruction)) = iter.next() {
+		println!("execute: {:?} at {}", instruction, position);
+		match instruction {
+			Instruction::MoveRight => pointer += 1,
+
+			Instruction::MoveLeft => {
+				if pointer == 0 {
+					pointer = usize::max_value();
+				} else {
+					pointer -= 1;
+				}
+			},
+
+			Instruction::Increment => memory[pointer] += 1,
+
+			Instruction::Decrement => {
+				if memory[pointer] == 0 {
+					memory[pointer] = u8::max_value();
+				} else {
+					memory[pointer] -= 1;
+				}
+			},
+
+			Instruction::Output => print!("{:#?}", memory[pointer]),
+
+			Instruction::Input => panic!("Unsupported operation {:?} at position {}", instruction, position),
+
+			_ => println!("??? {:?} at {}", instruction, position)
+		}
+	}
 
 	println!();
 }
