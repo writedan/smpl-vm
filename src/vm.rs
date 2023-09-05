094dc2b5f86c15691aca39df7b15876888830904
Daniel Write <daniel@writefamily.com>
2023-09-05 19:41:28 -0400

    added error handling

--- src/vm.rs
+++ src/vm.rs
@@ -16,15 +16,33 @@ pub mod vm {
 	} // more complex commands operate on the program
 
 	impl Program {
-		pub fn load(program: Vec<Token>) -> Program {
-			Program {
-				instructions: parse(program).unwrap(),
-				branches: BiMap::new(),
+		pub fn load(program: Vec<Token>) -> Result<Program, (String, usize, usize)> {
+			let instructions = match parse(program) {
+				Ok(instructions) => instructions,
+				Err((msg, line, chracter)) => return Err((msg, line, chracter))
+			};
+
+			let branches = match calculate_branches(&instructions) {
+				Ok(branches) => branches,
+				Err((msg, line, chracter)) => return Err((msg, line, chracter))
+			};
+
+			Ok(Program {
+				instructions: instructions,
+				branches: branches,
 				jumps: Vec::new(),
 				vm: Machine {
 					pointer: 0,
 					memory: Vec::new()
 				}
+			})
+		}
+
+		pub fn run(&mut self) {
+			let mut idx = 0;
+			while idx < self.instructions.len() {
+				println!("execute {:?}", self.instructions[idx]);
+				idx += 1;
 			}
 		}
 	}
