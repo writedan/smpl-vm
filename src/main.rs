4112fa7ce3cd59291c8491b4d12521aadc8cd7a8
Daniel Write <daniel@writefamily.com>
2023-09-05 01:10:11 -0400

    separated execution; loops function

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,26 @@
-mod parser;
-
 use std::env;
-use crate::parser::*;
+
+pub mod vm;
+pub mod parser;
+use crate::parser::parser::*;
+use crate::vm::vm::Program;
 
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 	let code = args[1].chars().collect::<Vec<char>>();
 
-	let mut memory: Vec<u8> = vec![0; u16::max_value() as usize];
+	let instructions: Vec<Instruction> = parse_instructions(code);
+	let branches = calculate_branches(&instructions).unwrap();
 
-	let instr: Vec<Instruction> = parse_instructions(code);
-	let branches = calculate_branches(&instr).unwrap();
+	let mut vm = Program::new(instructions, branches);
 
-	let mut ptr_jumps: Vec<usize> = vec![0, u16::max_value() as usize];
-	let mut pointer: usize = 0;
+	println!("Instructions: {:?}", vm.instructions);
+	println!("Branch table: {:?}", vm.branches);
 
-	println!("Instructions: {:?}", instr);
-	println!("Branch table: {:?}", branches);
+	println!("Begin execution:\n");
 
-	let mut iter = instr.iter().enumerate();
-	while let Some((position, instruction)) = iter.next() {
-		println!("execute: {:?} at {}", instruction, position);
-		match instruction {
-			Instruction::MoveRight => pointer += 1,
-
-			Instruction::MoveLeft => {
-				if pointer == 0 {
-					pointer = usize::max_value();
-				} else {
-					pointer -= 1;
-				}
-			},
-
-			Instruction::Increment => memory[pointer] += 1,
-
-			Instruction::Decrement => {
-				if memory[pointer] == 0 {
-					memory[pointer] = u8::max_value();
-				} else {
-					memory[pointer] -= 1;
-				}
-			},
-
-			Instruction::Output => print!("{:#?}", memory[pointer]),
-
-			Instruction::Input => panic!("Unsupported operation {:?} at position {}", instruction, position),
-
-			_ => println!("??? {:?} at {}", instruction, position)
-		}
-	}
+	vm.run();
 
 	println!();
 }
---
+++ src/vm.rs
@@ -0,0 +1,82 @@
+pub mod vm {
+	use crate::parser::parser::Instruction;
+	use bimap::BiMap;
+
+	struct Vm {
+		memory: Vec<u8>,
+		jumps: Vec<usize>,
+		pointer: usize
+	}
+
+	pub struct Program {
+		pub instructions: Vec<Instruction>,
+		pub branches: BiMap<usize, usize>,
+		machine: Vm
+	}
+
+	impl Default for Vm {
+		fn default() -> Vm {
+			return Vm {
+				memory: vec![0; u16::max_value() as usize],
+				jumps: Vec::new(),
+				pointer: 0
+			}
+		}
+	}
+
+	impl Program {
+		pub fn new(instructions: Vec<Instruction>, branches: BiMap<usize, usize>) -> Program {
+			return Program {
+				instructions: instructions,
+				branches: branches,
+				machine: Vm::default()
+			}
+		}
+
+		pub fn run(&mut self) {
+			let mut idx = 0;
+			while idx < self.instructions.len() {
+				let i = &self.instructions[idx];
+				match i {
+					Instruction::MoveRight => self.machine.pointer += 1,
+
+					Instruction::MoveLeft => {
+						if self.machine.pointer == 0 {
+							self.machine.pointer = usize::max_value();
+						} else {
+							self.machine.pointer -= 1;
+						}
+					},
+
+					Instruction::Increment => self.machine.memory[self.machine.pointer] += 1,
+
+					Instruction::Decrement => {
+						if self.machine.memory[self.machine.pointer] == 0 {
+							self.machine.memory[self.machine.pointer] = u8::max_value();
+						} else {
+							self.machine.memory[self.machine.pointer] -= 1;
+						}
+					},
+
+					Instruction::Output => print!("{:?}", self.machine.memory[self.machine.pointer]),
+
+					Instruction::Loop => {
+						if self.machine.memory[self.machine.pointer] == 0 {
+							idx = self.branches.get_by_left(&idx).unwrap() - 1;
+						} // otherwise do nothing
+					},
+
+					Instruction::Continue => {
+						if self.machine.memory[self.machine.pointer] != 0 {
+							idx = self.branches.get_by_right(&idx).unwrap() - 1;
+						} // otherwise do nothing
+					},
+
+					_ => println!("{:?}", i)
+				}
+
+				idx += 1;
+			}
+		}
+	}
+}
