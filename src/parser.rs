427908784e35833e095720c56127420425fbb78c
Daniel Write <daniel@writefamily.com>
2023-09-05 18:50:58 -0400

    added branch calculation

--- src/parser.rs
+++ src/parser.rs
@@ -105,7 +105,44 @@ pub mod parser {
 		return num;
 	}
 
-	pub fn calculate_branches(instr: Vec<Instruction>) -> Result<BiMap<usize, usize>, String> {
-		return Err(format!("unsupported"));
+	pub fn calculate_branches(instr: &Vec<Instruction>) -> Result<BiMap<usize, usize>, (String, usize, usize)> {
+		let mut branches: BiMap<usize, usize> = BiMap::new();
+
+		for (position, instruction) in instr.iter().enumerate() {
+			if discriminant(instruction) == discriminant(&Instruction::Branch(Token::Nop(0,0))) {
+				let open = position;
+				let mut offset = 0;
+				for (position, instruction) in instr[position + 1..].iter().enumerate() {
+					if discriminant(instruction) == discriminant(&Instruction::Branch(Token::Nop(0,0))) {
+						offset += 1;
+					} else if discriminant(instruction) == discriminant(&Instruction::Return(Token::Nop(0,0))) {
+						if offset == 0 {
+							branches.insert(open, position + open + 1);
+							break;
+						} else {
+							offset -= 1;
+						}
+					}
+				}
+
+				if !branches.contains_left(&open) {
+					if let Instruction::Branch(token) = instruction {
+						if let Token::Branch(line, character) = token {
+							return Err((format!("Branch ('[') defined on line {} at {} has no return (']')", line, character), *line, *character));
+						}
+					}
+				}
+			} else if discriminant(instruction) == discriminant(&Instruction::Return(Token::Nop(0,0))) {
+				if !branches.contains_right(&position) {
+					if let Instruction::Return(token) = instruction {
+						if let Token::Return(line, character) = token {
+							return Err((format!("Return (']') defined on line {} at {} has no branch ('[')", line, character), *line, *character));
+						}
+					}
+				}
+			}
+		}
+
+		return Ok(branches);
 	}
 }
