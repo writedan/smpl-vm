15e94b738a84e4f4a7c6bfc90208402c0cb132db
Daniel Write <daniel@writefamily.com>
2023-09-04 23:22:50 -0400

    split into separate parser, added support for branch

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,23 @@
-use std::env;
+mod parser;
 
-#[derive(Debug)]
-enum Instruction {
-	MoveRight,		// increment the pointer by one
-	MoveLeft,		// decrement the pointer by one
-	Increment,		// increment the memory at pointer by one
-	Decrement,		// decrement the memory at pointer by one
-	Output,			// output byte from memory
-	Input,			// input byte into memory
-	Loop,			// jump past the matching ] if memory is zero
-	Continue,		// jump to matching [ if memory is nonzero
-	Jump,			// set pointer to value in memory
-	Return,			// restore pointer to value before jump
-	Alloc,			// with the value of memory, jump to first instance of that many 0s in memory
-	Nop				// no operation
-}
+use std::env;
+use crate::parser::*;
+
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	let code = &args[1];
+	let code = args[1].chars().collect::<Vec<char>>();
 
 	let mut memory: Vec<u8> = vec![0; u16::max_value() as usize];
-	let mut instr: Vec<Instruction> = Vec::with_capacity(code.len());
 
-	for instruction in code.chars() {
-		let instruction = match instruction {
-			'>' => Instruction::MoveRight,
-			'<' => Instruction::MoveLeft,
-			'+' => Instruction::Increment,
-			'-' => Instruction::Decrement,
-			'.' => Instruction::Output,
-			',' => Instruction::Input,
-			'[' => Instruction::Loop,
-			']' => Instruction::Continue,
-			'*' => Instruction::Jump,
-			'&' => Instruction::Return,
-			'?' => Instruction::Alloc,
-			_ => Instruction::Nop // specification defiens other symbols to be ignored
-		};
-
-		instr.push(instruction);
-	}
+	let mut instr: Vec<Instruction> = parse_instructions(code);
+	let branches = calculate_branches(&instr).unwrap();
 
 	let mut ptr_jumps: Vec<usize> = vec![0, u16::max_value() as usize];
 	let mut pointer: usize = 0;
 
-	println!("{:?}", instr);
+	println!("Instructions: {:?}", instr);
+	println!("Branch table: {:?}", branches);
 
-
-	for instruction in instr {
-		match instruction {
-			Instruction::MoveRight => pointer += 1,
-			Instruction::MoveLeft => match pointer {
-				0 => pointer = usize::max_value(),
-				_ => pointer -= 1
-			},
-			Instruction::Increment => memory[pointer] += 1,
-			Instruction::Decrement => match memory[pointer] {
-				0 => memory[pointer] = u8::max_value(),
-				_ => memory[pointer] -= 1
-			},
-			Instruction::Output => println!("{:?}", memory[pointer] as char),
-			Instruction::Jump => {
-				ptrJumps.push(pointer);
-				pointer = memory[pointer] as usize;
-			},
-			Instruction::Return => {
-				pointer = ptr_jumps.pop().unwrap();
-			},
-			_ => pointer += 0
-		}
-	}
+	println!();
 }
---
+++ src/parser.rs
@@ -0,0 +1,82 @@
+use bimap::BiMap;
+
+#[derive(Debug, PartialEq)]
+pub enum Instruction {
+	MoveRight,		// increment the pointer by one
+	MoveLeft,		// decrement the pointer by one
+	Increment,		// increment the memory at pointer by one
+	Decrement,		// decrement the memory at pointer by one
+	Output,			// output byte from memory
+	Input,			// input byte into memory
+	Loop,			// jump past the matching ] if memory is zero
+	Continue,		// jump to matching [ if memory is nonzero
+	Jump,			// set pointer to value in memory
+	Return,			// restore pointer to value before jump
+	Alloc,			// with the value of memory, jump to first instance of that many 0s in memory
+	Nop				// no operation
+}
+
+impl PartialEq<Option<Instruction>> for &Instruction {
+	fn eq(&self, other: &Option<Instruction>) -> bool {
+        match other {
+            Some(other) => return other == *self,
+            None => return false,
+        }
+    }
+}
+
+pub fn parse_instructions(commands: Vec<char>) -> Vec<Instruction> {
+	let mut instr: Vec<Instruction> = Vec::with_capacity(commands.len());
+
+	for command in commands {
+		instr.push(match command {
+			'>' => Instruction::MoveRight,
+			'<' => Instruction::MoveLeft,
+			'+' => Instruction::Increment,
+			'-' => Instruction::Decrement,
+			'.' => Instruction::Output,
+			',' => Instruction::Input,
+			'[' => Instruction::Loop,
+			']' => Instruction::Continue,
+			'*' => Instruction::Jump,
+			'&' => Instruction::Return,
+			'?' => Instruction::Alloc,
+			_ => Instruction::Nop
+		});
+	}
+
+	return instr;
+}
+
+pub fn calculate_branches(instr: &Vec<Instruction>) -> Result<BiMap<usize, usize>, String> {
+	let mut branches: BiMap<usize, usize>  = BiMap::new(); // <open, close>
+
+	for (position, instruction) in instr.iter().enumerate() {
+		if instruction == &Instruction::Loop {
+			let open = position;
+			let mut offset = 0;
+			for (position, instruction) in instr[position + 1..].iter().enumerate() {
+				if instruction == &Instruction::Loop {
+					offset += 1;
+				} else if instruction == &Instruction::Continue {
+					if offset == 0 {
+						branches.insert(open, position + open + 1);
+						break;
+					} else {
+						offset -= 1;
+					}
+				}
+			}
+
+			if !branches.contains_left(&open) {
+				return Err(format!("Loop defined at instruction position {} has no continue.", open));
+			}
+		} else if instruction == &Instruction::Continue {
+			if !branches.contains_right(&position) {
+				return Err(format!("Continue defined at instruction position {} has no loop.", position));
+			}
+		}
+	}
+
+	return Ok(branches);
+}
